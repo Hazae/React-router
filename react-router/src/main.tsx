1001e6021dd4f9dd6d3ce00fe246e6183c61490b
yena kwon <ewko0220@gmail.com>
2023-08-10 21:40:53 +0900

    fix: 전체 컴포넌트 렌더링
    - pathname에 해당하는 페이지 렌더링

--- react-router/src/main.tsx
+++ react-router/src/main.tsx
@@ -3,8 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Router from "@components/Router/Router";
+import Routes from "@components/Routes/Routes";
+import Route from "@components/Route/Route";
+import Root from "@pages/Root/Root";
+import About from "@pages/About/About";
+
+const container = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(container).render(
+  <Router>
+    <Routes>
+      <Route path="/" component={<Root />}></Route>
+      <Route path="/about" component={<About />}></Route>
+    </Routes>
+  </Router>
 );
