36c31adf02e32e7803692db901c82a4621ae1a4e
yena kwon <ewko0220@gmail.com>
2023-08-10 13:16:50 +0900

    컴포넌트 만들기
    - Route

--- react-router/src/components/Route/Route.tsx
+++ react-router/src/components/Route/Route.tsx
@@ -4,3 +4,10 @@ export interface RouteProps {
   path: string;
   component: ReactNode;
 }
+
+// 로직 없는 껍데기
+const Route = ({ component }: RouteProps) => {
+  return <>{component}</>;
+};
+
+export default Route;
